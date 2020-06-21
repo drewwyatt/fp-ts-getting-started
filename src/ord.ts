cb5208097cc95fd3ac037f20e6bdf58bfee61096
Drew Wyatt <drew.j.wyatt@gmail.com>
2020-06-21 17:33:37 -0400

    ord

--- src/ord.ts
+++ src/ord.ts
@@ -1,7 +1,17 @@
 // https://dev.to/gcanti/getting-started-with-fp-ts-ord-5f1e
-import { contramap, ordNumber } from 'fp-ts/lib/Ord'
+import { Ord, contramap, getDualOrd, min, ordNumber } from 'fp-ts/lib/Ord'
 import { User, users, logger } from './utils'
 const log = logger('ord')
 
 const byAge = contramap(({ age }: User) => age)(ordNumber)
 log('byAge', users.sort(byAge.compare))
+
+const getYounger = min(byAge)
+log('getYounger', getYounger(users[0], users[1]))
+log('getYounger', getYounger(users[1], users[0])) // same as ^
+
+const max = <A>(ord: Ord<A>): ((a: A, b: A) => A) => min(getDualOrd(ord))
+const getOlder = max(byAge)
+
+log('getOlder', getOlder(users[0], users[1]))
+log('getOlder', getOlder(users[1], users[0])) // same as ^
