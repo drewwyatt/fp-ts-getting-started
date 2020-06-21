1d833a5f690afe8babfb321d6d6d5058db8f3e80
Drew Wyatt <drew.j.wyatt@gmail.com>
2020-06-21 15:56:45 -0400

    WIP: getting setup

---
+++ src/eq.ts
@@ -0,0 +1,13 @@
+// https://dev.to/gcanti/getting-started-with-fp-ts-setoid-39f3
+import type { Eq } from 'fp-ts/lib/Eq'
+
+function elem<A>(E: Eq<A>): (a: A, as: Array<A>) => boolean {
+  return (a: A, as: Array<A>) => as.some((b) => E.equals(a, b))
+}
+
+const eqNum: Eq<number> = {
+  equals: (a, b) => a === b,
+}
+
+console.log(elem(eqNum)(1, [1, 2, 3, 4]))
+console.log(elem(eqNum)(1, [2, 2, 3, 4]))
